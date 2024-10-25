688216996debe39d8016dd881d8d2cab84ec5476
Hikari <158005693+webHikari@users.noreply.github.com>
2024-10-26 00:01:22 +0300

    Update expiration time at cert.go

--- cert.go
+++ cert.go
@@ -59,7 +59,7 @@ func (m *mkcert) makeCert(hosts []string) {
 	// Certificates last for 2 years and 3 months, which is always less than
 	// 825 days, the limit that macOS/iOS apply to all certificates,
 	// including custom roots. See https://support.apple.com/en-us/HT210176.
-	expiration := time.Now().AddDate(2, 3, 0)
+	expiration := time.Now().AddDate(0, 0, 397)
 
 	tpl := &x509.Certificate{
 		SerialNumber: randomSerialNumber(),
